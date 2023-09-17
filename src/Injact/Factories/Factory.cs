446a78ad8e0a7eb0107401069828f6ed9145e49c
Jack Reimers <jack.reimers.00@gmail.com>
2023-09-17 21:19:33 +1000

    Make factory create method virtual

--- src/Injact/Factories/Factory.cs
+++ src/Injact/Factories/Factory.cs
@@ -2,9 +2,9 @@
 
 public class Factory<TValue> : IFactory<TValue>
 {
-    [Inject] private readonly DiContainer _container;
+    [Inject] private readonly DiContainer _container = null!;
 
-    public TValue Create()
+    public virtual TValue Create()
     {
         return _container.Resolve<TValue>(null);
     }
