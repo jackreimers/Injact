50da0fc997d025a7c4d4020f80edf9aec9efb31c
Jack Reimers <jack.reimers.00@gmail.com>
2023-12-23 20:58:43 +1000

    Fix file location

---
+++ src/Injact.Generators/ProxyGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Injact.Generators
+{
+    [Generator]
+    public class ProxyGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            
+        }
+    }
+}
